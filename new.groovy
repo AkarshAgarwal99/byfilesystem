8ed35c2bbbc76b65c354da2f0af72647a9f32ed4
Akarsh <akarshagarwal99@gmail.com>
2020-06-06 07:14:08 +0530

    aknfak

--- new.groovy
+++ new.groovy
@@ -1,17 +1,3 @@
-a=10
-if(a==10)
-{
-	job("myfirstjob"){
+job("myfirstjob"){
 		description("this is my discription")
-		
-		scm{
-			github("AkarshAgarwal99/byfilesystem.git")
-			}
-		triggers{
-			scm("* * * * *")
-			}
-		steps{
-			docker run -dit --name akarsh centos:latest
-			}
 		}
-}
